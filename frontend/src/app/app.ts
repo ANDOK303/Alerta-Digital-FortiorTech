0ab60b5eac8fa408ebe21d6c695fab14739c2143
ANDOK303 <agonzalez-2025464@kinal.edu.gt>
2026-09-24 17:02:45 -0600

    arreglos menores en app frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes';
 
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()), 
+    provideClientHydration()      
+  ]
+};
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], // ¡Esto es clave para que funcionen las rutas!
+  imports: [RouterOutlet], 
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
