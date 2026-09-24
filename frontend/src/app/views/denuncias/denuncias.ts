644bb121bfa03a7eea7a948d6c80b78a8c0360a2
rbran2026 <randolfbranreyes@gmail.com>
2026-09-24 15:50:29 -0600

    Se realizo la paguina html y css de denuncias

--- frontend/src/app/views/denuncias/denuncias.ts
+++ frontend/src/app/views/denuncias/denuncias.ts
@@ -1,53 +1,100 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
+
   selector: 'app-denuncias',
+
   standalone: true,
+
   imports: [CommonModule, FormsModule],
+
   templateUrl: './denuncias.html',
-  styleUrls: ['./denuncias.css']
+
+  styleUrl: './denuncias.css'
+
 })
+
 export class Denuncias implements OnInit {
+
   esAdmin = false;
+
   denuncias: any[] = [];
+
   nuevaDenuncia: any = {};
+ 
   constructor(
+
     @Inject(PLATFORM_ID) private platformId: Object,
+
     private http: HttpClient
+
   ) {}
  
   ngOnInit() {
+
     if (isPlatformBrowser(this.platformId)) {
+
       const usuarioGuardado = localStorage.getItem('usuario');
+
       if (usuarioGuardado) {
+
         const usuario = JSON.parse(usuarioGuardado);
+
         this.esAdmin = usuario.rol === 'administrador';
+
       }
+
     }
+
+  }
+ 
+  guardarDenuncia() {
+
+    this.http.post<any>('http://localhost:3000/api/denuncias', this.nuevaDenuncia)
+
+      .subscribe({
+
+        next: (respuesta) => {
+
+          this.denuncias.push(respuesta);
+
+          this.nuevaDenuncia = {};
+
+        },
+
+        error: (error) => {
+
+          console.error('Error al guardar denuncia', error);
+
+        }
+
+      });
+
+  }
+ 
+  actualizarEstado(d: any) {
+
+    this.http.patch(`http://localhost:3000/api/denuncias/${d.id_denuncia}/estado`, {
+
+      estado_denuncia: d.estado_denuncia
+
+    }).subscribe({
+
+      error: (error) => {
+
+        console.error('Error al actualizar estado', error);
+
+      }
+
+    });
+
   }
 
-  guardarDenuncia() {
-    this.http.post<any>('http://localhost:3000/api/denuncias', this.nuevaDenuncia)
-      .subscribe({
-        next: (respuesta) => {
-          this.denuncias.push(respuesta);
-          this.nuevaDenuncia = {};
-        },
-        error: (error) => {
-          console.error('Error al guardar denuncia', error);
-        }
-      });
-  }
-  actualizarEstado(d: any) {
-    this.http.patch(`http://localhost:3000/api/denuncias/${d.id_denuncia}/estado`, {
-      estado_denuncia: d.estado_denuncia
-    }).subscribe({
-      error: (error) => {
-        console.error('Error al actualizar estado', error);
-      }
-    });
-  }
 }
+ 
