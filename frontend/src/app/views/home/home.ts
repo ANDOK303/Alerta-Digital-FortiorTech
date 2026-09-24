5ef3eaa146d1e8d82ebabca3a1489add38966495
rbran2026 <randolfbranreyes@gmail.com>
2026-09-24 16:43:41 -0600

    se realizaron actualizaciones a la home

--- frontend/src/app/views/home/home.ts
+++ frontend/src/app/views/home/home.ts
@@ -1,34 +1,38 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+ 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class Home {
   estadoSistema: string = 'SEGURO';
   alertasPendientes: number = 0;
-
+ 
   constructor(private router: Router) {}
-
-  // Funciones infalibles para navegar por código
+ 
+  irADenuncias() {
+    this.router.navigate(['/denuncias']);
+  }
+ 
   irAPruebas() {
     this.router.navigate(['/pruebas-denuncia']);
   }
-
+ 
   irAForo() {
     this.router.navigate(['/publicaciones-foro']);
   }
-
+ 
   irACalificaciones() {
     this.router.navigate(['/calificaciones-plataforma']);
   }
-
+ 
   cerrarSesion() {
     console.log('Cerrando sesión...');
     this.router.navigate(['/login']);
   }
 }
+ 
