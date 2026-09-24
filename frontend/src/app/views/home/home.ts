bc73b784e8dd5c05e80dbe690f648647ced80103
ANDOK303 <agonzalez-2025464@kinal.edu.gt>
2026-09-24 16:51:46 -0600

    arreglos de conflictos en home

--- frontend/src/app/views/home/home.ts
+++ frontend/src/app/views/home/home.ts
@@ -1,38 +1,37 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
- 
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive], // Importamos RouterLinkActive para que pinte de azul el menú actual
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class Home {
   estadoSistema: string = 'SEGURO';
   alertasPendientes: number = 0;
- 
-  constructor(private router: Router) {}
- 
+
+  constructor(private router: Router) { }
+
   irADenuncias() {
     this.router.navigate(['/denuncias']);
   }
- 
+
   irAPruebas() {
     this.router.navigate(['/pruebas-denuncia']);
   }
- 
+
   irAForo() {
     this.router.navigate(['/publicaciones-foro']);
   }
- 
+
   irACalificaciones() {
     this.router.navigate(['/calificaciones-plataforma']);
   }
- 
+
   cerrarSesion() {
     console.log('Cerrando sesión...');
     this.router.navigate(['/login']);
   }
 }
- 
